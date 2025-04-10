901f2ea8d45379a96406e953d51d132a833788ec
Saad Salman <saadsalman@saads-mbp.speedport.ip>
2025-04-10 02:14:37 +0200

    add player model file

---
+++ lib/models/player.dart
@@ -0,0 +1,31 @@
+class Player {
+  String? nickname;
+  String? socketId;
+  double? points;
+  String? playerType;
+
+  Player({
+    this.nickname,
+    this.socketId,
+    this.points,
+    this.playerType,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'nickname': nickname,
+      'socketId': socketId,
+      'points': points,
+      'playerType': playerType,
+    };
+  }
+
+  factory Player.fromMap(Map<String, dynamic> json) {
+    return Player(
+      nickname: json['nickname'] as String?,
+      socketId: json['socketId'] as String?,
+      points: (json['points'] as num?)?.toDouble(),
+      playerType: json['playerType'] as String?,
+    );
+  }
+}
